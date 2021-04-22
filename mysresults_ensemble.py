3c1d33cbf44c75c645c3957cc666ebe950a3cf83
kowshik thopalli <kthopall@asu.edu>
2021-04-22 15:42:33 -0700

    changes to result generation

--- mysresults_ensemble.py
+++ mysresults_ensemble.py
@@ -4,23 +4,23 @@ import numpy as np
 
 for test_env in range(4):
 
-    out_file = 'Inevnio_outputs_vlcs/env'+str(test_env)+'/indiv_ensemble_no_beta/out copy.txt'
+    out_file = 'Inevnio_withoutaug/TerraIncognita_M3/env'+str(test_env)+'/indiv_ensemble_no_beta/out copy.txt'
     out_df= pd.read_fwf(out_file)
-    out_df= out_df[out_df['epoch']>100]
+    out_df= out_df[out_df['epoch']>0]
 
     train_envs = np.setdiff1d(np.arange(4),test_env)
 
     train_out_split_names= ['env'+str(i)+'_out_acc' for i in train_envs]
 
     test_ensemble_name= 'env'+str(test_env)+'_in_ens_acc'
-    test_ensemble_out_name= 'env'+str(test_env)+'_out_ens_ac'
+    test_ensemble_out_name= 'env'+str(test_env)+'_out_ens_acc'
     epochs= out_df['epoch']
-    validation_acc= np.sum(np.stack([out_df[i].values for i in train_out_split_names]),axis=0)/3.
+    validation_acc= np.sum(np.stack([out_df[i].values for i in train_out_split_names]) ,axis=0)/3.
 
     max_val_idx= np.argmax(validation_acc)
     max_val_idx_1 = np.argmax( out_df[test_ensemble_out_name].values)
 
-    print('test_train_Domain_val','test_oracle',out_df[test_ensemble_name].iloc[max_val_idx],out_df[test_ensemble_name].iloc[max_val_idx_1])
+    print('test_train_Domain_val','test_oracle',out_df[test_ensemble_name].iloc[max_val_idx]*100,out_df[test_ensemble_name].iloc[max_val_idx_1]*100)
 
 
 
